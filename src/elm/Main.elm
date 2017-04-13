e7159705c6e51846a763bf836449884e91cd7fd6
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-04-13 09:38:40 +0300

    Add setup for program with navigation

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,56 @@
 module Main exposing (..)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
 
--- component import example
-import Components.Hello exposing ( hello )
+import Html exposing (Html, text)
+import Navigation
+import UrlParser as Url exposing ((</>), (<?>), s, int, stringParam, top)
 
 
--- APP
-main : Program Never Int Msg
+type alias Model =
+    { route : Maybe Route }
+
+
+type Route
+    = Home
+    | Editor
+
+
+type Msg
+    = UrlChange Navigation.Location
+
+
+route : Url.Parser (Route -> a) a
+route =
+    Url.oneOf
+        [ Url.map Home top
+        , Url.map Editor (s "editor")
+        ]
+
+
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Navigation.program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
--- MODEL
-type alias Model = Int
-
-model : number
-model = 0
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    ( Model (Url.parsePath route location), Cmd.none )
 
 
--- UPDATE
-type Msg = NoOp | Increment
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
-update : Msg -> Model -> Model
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    ( model, Cmd.none )
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+    text "Hello World"
